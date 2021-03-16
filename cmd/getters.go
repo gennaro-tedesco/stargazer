c300adc6d8ca3651c7747b91a1b39b3914b11dc3
Gennaro Tedesco <gennarotedesco@gmail.com>
2021-03-16 17:14:59 +0100

    added some more getters

--- cmd/getters.go
+++ cmd/getters.go
@@ -10,19 +10,47 @@ import (
 )
 
 type RepoJson struct {
-	Forks int    `json:"forks"`
-	Stars int    `json:"stargazers_count"`
 	Name  string `json:"name"`
+	Stars int    `json:"stargazers_count"`
+	Forks int    `json:"forks"`
+	SvnUrl string `json:"svn_url"`
 }
 
-func getStars(userName string) {
-	var repos = getRepoList(userName)
+func getStars(repos []RepoJson) {
 	t := table.NewWriter()
     t.SetOutputMirror(os.Stdout)
-    t.AppendHeader(table.Row{"Repo name", "Repo stars"})
+    t.AppendHeader(table.Row{"name", "stars"})
 	for _, repo := range repos{
 		t.AppendRows([]table.Row{
 			{repo.Name, repo.Stars},
+		})
+	}
+    t.AppendSeparator()
+	t.SetStyle(table.StyleLight)
+    t.Render()
+}
+
+func getForks(repos []RepoJson) {
+	t := table.NewWriter()
+    t.SetOutputMirror(os.Stdout)
+    t.AppendHeader(table.Row{"name", "forks"})
+	for _, repo := range repos{
+		t.AppendRows([]table.Row{
+			{repo.Name, repo.Forks},
+		})
+	}
+    t.AppendSeparator()
+	t.SetStyle(table.StyleLight)
+    t.Render()
+}
+
+func getUrl(repos []RepoJson) {
+	t := table.NewWriter()
+    t.SetOutputMirror(os.Stdout)
+    t.AppendHeader(table.Row{"name", "url"})
+	for _, repo := range repos{
+		t.AppendRows([]table.Row{
+			{repo.Name, repo.SvnUrl},
 		})
 	}
     t.AppendSeparator()
@@ -53,7 +81,7 @@ func getRepoList(userName string) []RepoJson {
 
 	var repos []RepoJson
 	if err := json.Unmarshal(responseBytes, &repos); err != nil {
-		fmt.Println("could not return unmarshall: %v", err)
+		fmt.Printf("could not return unmarshall: %v", err)
 	}
 
 	return repos
--- cmd/root.go
+++ cmd/root.go
@@ -3,52 +3,21 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "stargazer",
 	Short: "list github repositories with star count",
-	Long: "list github repositories with star count",
+	Long:  "list github repositories with star count",
 	Run: func(cmd *cobra.Command, args []string) {
-		getStars("gennaro-tedesco")
+		var repos = getRepoList("gennaro-tedesco")
+		getStars(repos)
+		getForks(repos)
+		getUrl(repos)
 	},
 }
 
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.stargazer.yaml)")
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".stargazer")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-}
-
